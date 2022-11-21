c4ea91a95d73dcf47f8eb8d266d13301b8642704
RoSteinberg <rsteinbe@uni-bremen.de>
2022-11-21 14:42:39 +0100

    ggplot exercise 3

--- course_material/exercises/plots_3_colours.R
+++ course_material/exercises/plots_3_colours.R
@@ -19,11 +19,33 @@ penguins <- penguins
 
 # Example -----------------------------------------------------------------
 
+# continuos viridis colour palette
+ggplot(data = penguins,
+       aes(x = body_mass_g, y = bill_length_mm)) +
+  geom_point(aes(colour = bill_depth_mm))+
+  #scale_colour_viridis_c(option = "A")
+  #scale_color_distiller(palette = 'Set1')
+  scale_colour_gradientn(colours = c('#e87278','#5e8436','#8771de','#c0ae48','#c73762','#b25626','#c33a39'))
+
 # Discrete viridis colour palette
 ggplot(data = penguins,
        aes(x = body_mass_g, y = bill_length_mm)) +
-  geom_point(aes(colour = as.factor(year))) +
-  scale_colour_viridis_d(option = "A")
+  geom_point(aes(colour = as.factor(sex))) +
+  #scale_colour_viridis_d(option = "D")
+  #scale_colour_brewer(palette = 'Set1')
+  scale_color_manual(values = c('#e87278', '#5e8436'),
+                     labels = c('female', 'male', 'other'))+
+  labels(colour = 'Sex')
+
+ggplot(data = penguins,
+       aes(x = body_mass_g, y = bill_length_mm)) +
+  geom_point(aes(colour = as.factor(sex))) +
+  # How to use custom palette
+  scale_colour_manual(values = c('#e87278', '#5e8436'),
+                      # How to change the legend text
+                      labels = c("female", "male", "other")) + 
+  # How to change the legend title
+  labs(colour = "Sex") 
 
 # Compare species
 ggplot(data = penguins, aes(x = species, y = bill_length_mm)) +
@@ -31,11 +53,55 @@ ggplot(data = penguins, aes(x = species, y = bill_length_mm)) +
   stat_compare_means(method = "anova")
 
 
+# First create a list of comparisons to feed into our figure
+penguins_levels <- levels(penguins$species)
+my_comparisons <- list(c(penguins_levels[1], penguins_levels[2]), 
+                       c(penguins_levels[2], penguins_levels[3]),
+                       c(penguins_levels[1], penguins_levels[3]))
+
+# Then we stack it all together
+ggplot(data = penguins, aes(x = species, y = bill_length_mm)) +
+  geom_boxplot(aes(fill  = species), colour = "grey40", show.legend = F) +
+  stat_compare_means(method = "anova", colour = "grey50",
+                     label.x = 1.8, label.y = 32) +
+  # Add pairwise comparisons p-value
+  stat_compare_means(comparisons = my_comparisons,
+                     label.y = c(62, 64, 66)) +
+  # Perform t-tests between each group and the overall mean
+  stat_compare_means(label = "p.signif", 
+                     method = "t.test",
+                     ref.group = ".all.") + 
+  # Add horizontal line at base mean
+  geom_hline(yintercept = mean(penguins$bill_length_mm, na.rm = T), 
+             linetype = 2) + 
+  labs(y = "Bill length (mm)", x = NULL) +
+  theme_bw()
 # Exercise 1 --------------------------------------------------------------
 
 # Create your own continuous and discrete colour palettes
 # Create and combine two figures, each using a different palette
 
+lm_1 <-
+  ggplot(data = penguins,
+       aes(x = body_mass_g, y = bill_length_mm)) +
+  geom_point(aes(colour = as.factor(species)))+
+  scale_colour_manual(values = c('#e87278', '#5e8436', '#8771de'),
+                      # How to change the legend text
+                      labels = c("Adelie", "Chinstrap", "Gentoo"))+
+  labs(x  = 'Body mass (g)', y  = 'Bill length (mm)', colour = 'Species')+
+  theme_bw()
+
+lm_2 <-
+  ggplot(data = penguins,
+       aes(x = bill_depth_mm, y = bill_length_mm)) +
+  geom_point(aes(colour = as.factor(sex)))+
+  scale_colour_manual(values = c('#c0ae48','#b25626'),
+                      # How to change the legend text
+                      labels = c("Female", "Male", "other"))+
+  labs(x  = 'Bill length (mm)', y  = 'Bill length (mm)', colour = 'Sex')+
+  theme_bw()
+
+ggarrange(lm_1, lm_2)
 
 # Exercise 2 --------------------------------------------------------------
 
